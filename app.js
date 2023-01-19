7fe6026fb1191c83cae0e04d8a91a1e253f6ec92
David Jeong <davidjeong221@gmail.com>
2023-01-19 00:17:31 -0500

    removed unnecessary ] character

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ new Set([1, 1, 2, 2, 3, 4]);
 let m = new Map();
 m.set([1, 2, 3], true);
 m.set([1, 2, 3], false);
-// {Array(3) => true, Array(3) => false]}
+// {Array(3) => true, Array(3) => false}
 
 
 // Write a function called hasDuplicate which accepts an array and returns true or false if that array contains a duplicate
